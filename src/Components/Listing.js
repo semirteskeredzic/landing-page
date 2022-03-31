772bb759b7d345f8a0a7acedb19b59fb24191204
semirteskeredzic <semir.teskeredzic@gmail.com>
2022-03-31 14:15:45 +0200

    Added radio filter; created Services for fetching data; Provided child component for product listing

--- src/Components/Listing.js
+++ src/Components/Listing.js
@@ -1,6 +1,8 @@
 import { CSpinner } from "@coreui/react";
 import React, { useEffect, useRef, useState } from "react";
-import { getProducts } from "../Services/ProductsService";
+import { getProducts, getCategory } from "../Services/ProductsService";
+import ListingActions from "./ListingActions";
+import ListingProducts from "./ListingProducts";
 
 const Listing = () => {
 
@@ -8,51 +10,42 @@ const Listing = () => {
     const [products, setProducts] = useState([])
     const [productLimit, setProductLimit] = useState(8)
     const [totalProducts, setTotalProducts] = useState(32)
-
-    console.log('productLimit', productLimit)
-    console.log('totalProducts', totalProducts)
+    const [selectedCategories, setSelectedCategories] = useState([])
+    const [filtered, setFiltered] = useState(false)
 
     const endRef = useRef(null)
 
-    const scrollToBottom = () => endRef.current.scrollIntoView({ behavior: "smooth" })
-        
     useEffect(() => {
-        setLoading(true)
+        if(selectedCategories.length === 0 || !filtered) {
+            setLoading(true)
         getProducts(productLimit)
         .then(res => setProducts(res.data.products))
         .finally(
-            productLimit > 8 ? scrollToBottom() : null,
             setLoading(false)
         )
-    }, [productLimit])
+        }
+    }, [productLimit, filtered])
 
-    if (loading || !products) return <CSpinner />
+    useEffect(() => {
+        console.log('selectedCategories', selectedCategories.length)
+        if (selectedCategories.length !== 0) {            
+            setLoading(true)
+            getCategory(selectedCategories)
+            .then(res => setProducts(res.data.products), setLoading(false))
+        }
+    },[selectedCategories])
 
     return (
         <section>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {!products.length ? <CSpinner /> : 
-                (products?.map(product => (
-                    <div key={product.id} className="border-4 h-[500px] rounded-md p-4 m-4">
-                        <div className="m-0 m-auto h-2/4">
-                            <img className="m-0 m-auto max-h-full" src={product.images[0]} alt={product.name} />
-                        </div>
-                        <div className="h-1/4 mt-3">
-                            <p className="text-sm font-semibold italic text-gray-300">{product.category}</p>
-                            <h3 className="text-xl mb-2">{product.title}</h3>
-                            <p className="text-lg font-extrabold">{product.price}</p>     
-                        </div>
-                        <p className="h-[48px] mt-2 text-xs overflow-hidden">{product.description}</p>
-                        <button className="w-full mt-3 botom-0 shadow-sm rounded-md p-2 bg-blue-500 text-white hover:bg-blue-700">Detail</button>
-                    </div>
-            )))}
-        </div>
+        <ListingActions setfilteredcategories={setSelectedCategories} filter={setFiltered} />
+        <ListingProducts products={products} limit={productLimit} setLimit={setProductLimit} total={totalProducts} selectedcategories={selectedCategories} />
         <div ref={endRef} className="text-center">
-            {productLimit <= totalProducts ? 
+            {selectedCategories.length === 0 && 
+            (productLimit <= totalProducts ? 
             (<button onClick={() => setProductLimit(productLimit + 4)} className="px-6 py-4 uppercase rounded-md bg-blue-600 hover:bg-blue-800 text-white cursor-pointer">
                 {loading ? <CSpinner/> : 'Load More'}
             </button>)
-            : <button className="px-6 py-4 uppercase rounded-md bg-gray-300 text-white" disabled>Load More</button> }
+            : <button className="px-6 py-4 uppercase rounded-md bg-gray-300 text-white" disabled>Load More</button> )}
         </div>
         </section>
     );
